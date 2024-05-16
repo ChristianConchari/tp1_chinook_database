7599f3da3d747dc1c2743d55f0ee4257dcff65c8
Christian Conchari <chrisconchari@gmail.com>
2024-05-16 18:28:08 -0400

    feat: Add SQL solution script for exercise 16

---
+++ solution_scripts/exercise16.sql
@@ -0,0 +1,8 @@
+-- 16. Para cada factura, calcula la diferencia en el gasto total entre ella y la factura anterior.
+
+SELECT 
+    invoice_id, 
+    invoice_date, total,
+    LAG(total, 1) OVER (ORDER BY invoice_date ASC) AS previous_invoice_total,
+    total - LAG(total, 1) OVER (ORDER BY invoice_date ASC) as difference
+FROM invoice;
